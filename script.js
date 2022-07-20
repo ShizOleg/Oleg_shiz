983c508fc90a5c463c9204c9bc8a9267e3a408ea
VladislavPetrukhin <46006319+VladislavPetrukhin@users.noreply.github.com>
2022-07-20 17:21:25 +0300

    123

--- script.js
+++ script.js
@@ -19,3 +19,20 @@ const swiper = new Swiper('.swiper', {
         el: '.swiper-scrollbar',
     },
 });
+
+
+let button1 = document.getElementById("btn1");
+let button2 = document.getElementById("btn2");
+let button3 = document.getElementById("btn3");
+let button4 = document.getElementById("btn4");
+let button5 = document.getElementById("btn5");
+
+function clk() {
+    document.getElementById('active').checked = false;
+
+}
+button1.addEventListener("click", clk);
+button2.addEventListener("click", clk);
+button3.addEventListener("click", clk);
+button4.addEventListener("click", clk);
+button5.addEventListener("click", clk);
